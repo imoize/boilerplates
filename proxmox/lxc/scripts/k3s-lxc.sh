ef59dc774eeb55ca50e88ac1a72473447052ad8c
imoize <51510865+imoize@users.noreply.github.com>
2023-06-13 05:18:20 +0700

    Finalizing script

--- proxmox/lxc/scripts/k3s-lxc.sh
+++ proxmox/lxc/scripts/k3s-lxc.sh
@@ -2,11 +2,11 @@
 
 CONF_KMSG='/usr/local/bin/conf-kmsg.sh'
 SERVICE_KMSG='/etc/systemd/system/conf-kmsg.service'
-GPG='gpg'
-PKGS='curl apt-transport-https helm'
+REQUIRED='gpg curl apt-transport-https'
+PKGS='helm'
 INSTALL=false
-GREEN=$(tput setaf 2)
-CYAN=$(tput setaf 6)
+GR=$(tput setaf 2)
+CY=$(tput setaf 6)
 NC=$(tput sgr 0)
 
 usage() {
@@ -21,7 +21,7 @@ case "${1}" in
 esac
 done
 
-for pkg in $PKGS $GPG; do
+for pkg in $PKGS $REQUIRED; do
     status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
     if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
     INSTALL=true
@@ -30,33 +30,33 @@ fi
 done
 
 if [[ "${uninstall}" == 'true' ]]; then
-    echo "${GREEN}Uninstalling...${NC}"
-    sudo apt remove -y --purge $PKGS $GPG &>/dev/null
+    echo "${GR}Uninstalling...${NC}"
+    sudo apt remove -y --purge $PKGS &>/dev/null
     sudo apt autoremove -y &>/dev/null
     sudo rm -rf "$CONF_KMSG" "$SERVICE_KMSG" /usr/share/keyrings/helm.gpg /etc/apt/sources.list.d/helm-stable-debian.list
     systemctl daemon-reload
     sudo apt update &>/dev/null
 elif [ -f "$CONF_KMSG" ] && [ -x "$CONF_KMSG" ] && [ "$status" = installed ]; then
-    echo "${GREEN}Setup already done.${NC}"
+    echo "${GR}Setup already done.${NC}"
 else
-    echo "${GREEN}Setup requirement for k3s...${NC}"
+    echo "${GR}Setup requirement for k3s...${NC}"
 fi
 
 if [ "${uninstall}" == 'true' ] && [ ! -f "$CONF_KMSG" ] && [ ! -f "$SERVICE_KMSG" ]; then
-    echo "${GREEN}Uninstall done please reboot.${NC}"
+    echo "${GR}Uninstall done please reboot.${NC}"
 fi
 
 if [ "$INSTALL" ] && [ "${uninstall}" != 'true' ] && [ ! "$status" = installed ]; then
-    echo "${CYAN}Install dependencies...${NC}"
-    sudo apt install -y $GPG &>/dev/null   
-    sudo wget -qO- https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+    echo "${CY}Install dependencies...${NC}"
+    sudo apt install -y $REQUIRED &>/dev/null   
+    sudo curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
     sudo apt update &>/dev/null
     sudo apt install -y $PKGS &>/dev/null
 fi
 
 if [ ! -f "$CONF_KMSG" ] && [ "${uninstall}" != 'true' ]; then
-    echo "${CYAN}Create conf-kmsg.sh in /usr/local/bin/...${NC}"
+    echo "${CY}Create conf-kmsg.sh -> $CONF_KMSG${NC}"
     cat > "$CONF_KMSG" <<EOF
 #!/bin/sh -e
 
@@ -69,13 +69,13 @@ EOF
 fi
 
 if [ ! -x "$CONF_KMSG" ] && [ "${uninstall}" != 'true' ]; then
-    echo "${CYAN}Make conf-kmsg.sh executable...${NC}"
+    echo "${CY}Make conf-kmsg.sh executable...${NC}"
     chmod +x "$CONF_KMSG"
-        
+
 fi
 
 if [ ! -f "$SERVICE_KMSG" ] && [ "${uninstall}" != 'true' ]; then
-    echo "${CYAN}Create conf-kmsg.service in /etc/systemd/system/...${NC}"
+    echo "${CY}Create conf-kmsg.service -> $SERVICE_KMSG${NC}"
     cat > "$SERVICE_KMSG" <<EOF
 [Unit]
 Description=Make sure /dev/kmsg exists
@@ -89,5 +89,5 @@ WantedBy=default.target
 EOF
 	systemctl daemon-reload
     systemctl enable --now conf-kmsg
-    echo "${GREEN}Setup done please reboot.${NC}"
+    echo "${GR}Setup done please reboot.${NC}"
 fi
